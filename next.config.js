32b3029aa983fe23dbc4395cc301d8d8686e1e04
boulaoutaqh <Boulaoutaqh@gmail.com>
2026-06-12 12:30:18 +0200

    fix: redirect www to non-www

--- next.config.js
+++ next.config.js
@@ -1,14 +1,20 @@
-/** @type {import('next').NextConfig} */ 
-module.exports = { 
-  reactStrictMode: true, 
-  async redirects() { 
-    return [ 
-      { 
-        source: '/:path*', 
-        has: [{ type: 'host', value: 'subshed.vercel.app' }], 
-        destination: 'https://subshedapp.com/:path*', 
-        permanent: true, 
-      }, 
-    ]; 
-  }, 
-}; 
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'subshed.vercel.app' }],
+        destination: 'https://subshedapp.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.subshedapp.com' }],
+        destination: 'https://subshedapp.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
